10d05eb036647152842e3a653af8cfe73607fca5
Zoriyag <zoriyag@icloud.com>
2025-07-05 22:14:07 -0500

    Create affirmation_generator.py
    Add initial affirmation_generator.py script

---
+++ affirmation_generator.py
@@ -0,0 +1,12 @@
+import random
+
+affirmations = [
+    "You're doing better than you think.",
+    "One line of code at a time.",
+    "It’s okay to be a beginner.",
+    "Progress is progress.",
+    "Believe in your future self."
+]
+
+print("\n✨ Your Daily Affirmation ✨\n")
+print(random.choice(affirmations))
